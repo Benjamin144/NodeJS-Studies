4a677c80f1849510b8c5004dbbfd95d5b4ff34ae
Benjamin144 <josephroberts@email.com>
2024-06-14 20:02:32 +0000

    test fs to read txt file from index,js pathway

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/* const fs = require('fs');
+const fs = require("fs");
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn); */
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
 
-const hello = 'Hello world!';
+const hello = "Hello world!";
 console.log(hello);
